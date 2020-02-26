bc7f87366f20af4de375f21a21f8808d8668c555
IllAle <illikainen.aleksi@gmail.com>
2020-02-26 22:53:28 +0200

    Responsive design for mobile fixed, changed some color values, and added hamburger icon

--- Blog/webblog/models.py
+++ Blog/webblog/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
 	article_text = models.TextField()
 	article_date = models.DateTimeField()
 	article_image = models.ImageField(upload_to="images", blank=True)
+	article_high_res_image = models.ImageField(upload_to="images", blank=True)
 
 	def __str__(self):
 		return self.article_header
@@ -13,7 +14,9 @@ class Article(models.Model):
 class Comment(models.Model):
 	comment_date = models.DateTimeField()
 	comment_text = models.CharField(max_length=600)
+	comment_article_id = models.IntegerField()
 	commentor_name = models.CharField(max_length=50)
+
 
 	def __str__(self):
 		return self.commentor_name
